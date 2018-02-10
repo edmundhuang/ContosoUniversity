7c4ddf3b06e3d3e91366c69bffba5b70fed0c3a6
edmundhuang <changchun.huang@gmail.com>
2018-02-10 10:11:45 +0800

    完成第九章 实体继承

--- ContosoUniversity/Migrations/20180209092716_Inheritance.Designer.cs
+++ ContosoUniversity/Migrations/20180209092716_Inheritance.Designer.cs
@@ -13,9 +13,10 @@ using System;
 namespace ContosoUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180209092716_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
